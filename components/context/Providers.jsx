802c93ceba6d0a741905787b4aab32f2247b4fd6
Pimuk Artharnnarong <pimuk.artharnnarong@gmail.com>
2025-08-11 09:32:32 +0700

    refactor: comment out ThemeToggleButton import and usage for future consideration

--- components/context/Providers.jsx
+++ components/context/Providers.jsx
@@ -4,7 +4,7 @@ import { SessionProvider } from 'next-auth/react';
 import { ThemeProviderWrapper } from '@/theme/ThemeContext';
 import { ThemeProvider } from '@/theme';
 import { ModalProvider } from '@/components/modals/ModalProvider';
-import ThemeToggleButton from '@/theme/ThemeToggleButton';
+// import ThemeToggleButton from '@/theme/ThemeToggleButton';
 
 export function Providers({ children }) {
   return (
@@ -14,7 +14,7 @@ export function Providers({ children }) {
           <ModalProvider>
             {children}
           </ModalProvider>
-          <ThemeToggleButton />
+          {/* <ThemeToggleButton /> */}
         </ThemeProvider>
       </ThemeProviderWrapper>
     </SessionProvider>
